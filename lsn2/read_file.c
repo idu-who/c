3535eddb32f6fad455826e4dd32ff7c094322f3f
idu-who <104561260+idu-who@users.noreply.github.com>
2025-12-20 16:02:34 +0530

    closing the file
    Signed-off-by: idu-who <104561260+idu-who@users.noreply.github.com>

--- lsn2/read_file.c
+++ lsn2/read_file.c
@@ -13,12 +13,13 @@ int main() {
     char line[1000];
     FILE *hand = fopen("romeo.txt", "r");
 
-    if (hand == NULL) {
+    if (hand == NULL)
         printf("romeo.txt failed to open.\n");
-    } else {
-        while (fgets(line, 1000, hand) != NULL) {
+    else {
+        while (fgets(line, 1000, hand) != NULL)
             printf("%s", line);
-        }
+
+        fclose(hand);
     }
     return 0;
 }
